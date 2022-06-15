2e4cf88f92cd235ff6fb02e9e2c1d4a1f9aadc30
leslie2952 <85373184+Leslie144@users.noreply.github.com>
2022-06-15 11:32:27 -0500

    tarjeta

---
+++ src/main/java/pe/edu/upc/demo/serviceimplements/TarjetaServiceImpl.java
@@ -0,0 +1,29 @@
+package pe.edu.upc.demo.serviceimplements;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import pe.edu.upc.demo.entities.Tarjeta;
+import pe.edu.upc.demo.repositories.ITarjetaRepository;
+import pe.edu.upc.demo.serviceinterface.ITarjetaService;
+
+@Service
+public class TarjetaServiceImpl implements ITarjetaService {
+	@Autowired
+	private ITarjetaRepository tarjetaRepository;
+
+	@Override
+	public void insert(Tarjeta t) {
+		// TODO Auto-generated method stub
+		tarjetaRepository.save(t);
+	}
+
+	@Override
+	public List<Tarjeta> list() {
+		// TODO Auto-generated method stub
+		return tarjetaRepository.findAll();
+	}
+
+}
