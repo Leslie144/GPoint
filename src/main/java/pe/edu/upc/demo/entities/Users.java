e423dd4ca6da2987b49ecd3bdfe6d75a4eec301a
diego040502 <103788504+diego040502@users.noreply.github.com>
2022-06-22 23:47:41 -0500

    Add files via upload

---
+++ src/main/java/pe/edu/upc/demo/entities/Users.java
@@ -0,0 +1,83 @@
+package pe.edu.upc.demo.entities;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "users")
+public class Users {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotNull(message="El nombre de usuario es obligatorio")
+	@Pattern(regexp="[a-z0-9]")
+	@Column(length = 30, unique = true)
+	@Size(min=5, max=20, message="El username tiene como mínimo 5 letras y máximo 20")
+	private String username;
+
+	@NotNull(message="El password es obligatorio")
+	@Column(length = 200)
+	private String password;
+
+	private Boolean enabled;
+
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "user_id")
+	private List<Role> roles;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Boolean getEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(Boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public List<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
+
+}
